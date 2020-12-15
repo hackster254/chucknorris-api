f5c98d23eb0c029b8e29784be8502b26a8fedf24
hackster254 <43937812+hackster254@users.noreply.github.com>
2020-12-15 22:56:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-//import React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 // to use img you have to import it
 import Chuck from './chuck.png';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 function App() {
+
+
 
 // creata a state and call function to setstate
 const [state, setState] = useState({
   joke: '',
   searchedjoke: '',
-  //categories: []
+  categories: []
 })
 
-const [query, setQuery] = useState('teeth')
+const [query, setQuery] = useState('sport')
+const [querycat, setQuerycat] = useState("sport");
+
+const [data, setData] = useState([])
+const [q,setQ] =useState("")
+
+/*
+useEffect(()=> {
+  fetch("https://api.chucknorris.io/jokes/random?category={category}")
+    .then((response) => response.json() )
+    .then((json) => setData(json));
+
+}, []);
+*/
 
 
 
@@ -30,21 +49,40 @@ const [query, setQuery] = useState('teeth')
   const fetchData = async () =>{
     const result = await axios.get("https://api.chucknorris.io/jokes/random");
 
-    const response = await axios.get(
+    /*const response = await axios.get(
       `https://api.chucknorris.io/jokes/search?query=${query}`
     );
+    */
+
+    
+   const response = await axios.get(
+     `https://api.chucknorris.io/jokes/random?category=${query}`
+   );
    
+    const catResponse = await axios.get(
+      `https://api.chucknorris.io/jokes/random?category=${querycat}`
+    );
     console.log(result);
     console.log(response);
-    console.log(result.data.value);
+    //response.data.value
+    //console.log(result.data.value);
 
-    console.log('searched joke is'+ response.data.result[0].value)
+    //console.log('searched joke is'+ response.data.result[0].value)
+      console.log("searched joke is" + response.data.value);
+    console.log(catResponse)
+    console.log('categories is '+ catResponse.data.categories)
 
+
+  
+
+    ///
     //call state and update value 
     setState({
       ...state,
       joke: result.data.value,
-      searchedjoke: response.data.result[0].value,
+
+      //searchedjoke: response.data.result[0].value,
+      searchedjoke: response.data.value,
     });
   }
 
@@ -52,6 +90,7 @@ const [query, setQuery] = useState('teeth')
     e.preventDefault()
     fetchData();
   }
+ 
   // get categories
   /*
     const getCategories = async () => {
@@ -79,38 +118,53 @@ const [query, setQuery] = useState('teeth')
 
         <div className="col-6 searchJokeCol">
           <div className="card">
-            <div className="card-header">Search for a word</div>
+            <div className="card-header">
+              Search for a joke in a category e.g sport
+            </div>
             <form onSubmit={handleSearch}>
-              <div className="card-body">
-                <input
-                  type="text"
-                  onChange={(e) => setQuery(e.target.value)}
-                  value={query}
-                />
-              </div>
+                <div className="card-body">
+                  <input
+                    type="text"
+                    onChange={(e) => setQuery(e.target.value)}
+                    value={query}
+                  />
+                </div>
+                    
+                      <div className="card-body">
+                        <input
+                          type="button"
+                          onChange={(e) => setQuery(e.target.value)}
+                          value={query}
+                        />
+                      </div>
+
+                    <div>
+                      <button type="submit" className="btn btn-warning btn-lg">
+                        GENERATE JOKE
+                      </button>
+                    </div>
+              </form>
 
               <div>
-                <button type="submit" className="btn btn-warning btn-lg">
-                  GENERATE JOKE
+                <button className="btn btn-success btn-lg">
+                  <a href="">RANDOM</a>
                 </button>
               </div>
-            </form>
-
-            <div>
-              <button className="btn btn-success btn-lg">
-                <a href="">RANDOM</a>
-              </button>
             </div>
           </div>
         </div>
 
         <h2 className="subtitle">Here is the Joke</h2>
-        <h4>{state.joke}</h4>
+
         <break></break>
         <h4>{state.searchedjoke}</h4>
       </div>
-    </div>
+    
   );
+  
+
+  
 }
+
 
 export default App;
